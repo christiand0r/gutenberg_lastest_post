fa641e1ec2231ac5491d7d88c78e4a28c1bacb64
Christian Osorio <christiandoor.2001@gmail.com>
2023-06-26 20:04:36 -0400

    fix/ Rename data for latest posts block

--- main.php
+++ main.php
@@ -1,21 +1,21 @@
 <?php
 
 /**
- * Plugin Name:       Boilerplate
- * Description:       Write a description for block
+ * Plugin Name:       Latest Posts Block
+ * Description:       Display and filter latest posts
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            Boilerplate
+ * Author:            Christian Door
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       boilerplate
+ * Text Domain:       gutenberg
  *
- * @package           create-block
+ * @package           gutenberg
  */
 
-function create_block_boilerplate_block_init()
+function create_latest_posts_block_init()
 {
 	register_block_type(__DIR__ . '/build');
 }
-add_action('init', 'create_block_boilerplate_block_init');
+add_action('init', 'create_latest_posts_block_init');
